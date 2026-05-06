a0d987f9360c9a298d9cc8c77060532536704ed2
reeditor <reeditor.hub@gmail.com>
2026-05-06 18:39:30 +0300

    The feature that automatically launches the program when Windows starts has been implemented

---
+++ Services/Manager/AutoStartManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System.IO;
+
+namespace EchoVault.Services.Manager
+{
+    public static class AutoStartManager
+    {
+        private const string AppName = "EchoVault";
+
+        public static void SetState(bool enable)
+        {
+            string exePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         AppDomain.CurrentDomain.FriendlyName + ".exe");
+
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true))
+            {
+                if (key == null) return;
+
+                if (enable)
+                {
+                    key.SetValue(AppName, $"\"{exePath}\"");
+                }
+                else
+                {
+                    if (key.GetValue(AppName) != null)
+                    {
+                        key.DeleteValue(AppName);
+                    }
+                }
+            }
+        }
+
+        public static bool IsEnabled()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", false))
+            {
+                return key?.GetValue(AppName) != null;
+            }
+        }
+    }
+}
