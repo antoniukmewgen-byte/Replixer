a5160059953d9dff856ef28997ee20cc1ed80e2b
BitForge <antoniuk.m.ewgen@gmail.com>
2026-05-12 02:35:52 +0300

    FixTelegramDialog

--- Views/Dialogs/InputDialogView.xaml.cs
+++ Views/Dialogs/InputDialogView.xaml.cs
@@ -8,11 +8,10 @@ public partial class InputDialogView : UserControl
     public InputDialogView()
     {
         InitializeComponent();
-        Loaded += (_, _) => InputBox.Focus();
-        InputBox.KeyDown += OnKeyDown;
+        Loaded += (_, _) => TelegramVerification.Focus();
     }
 
-    private void OnKeyDown(object sender, KeyEventArgs e)
+    private void InputBox_KeyDown(object sender, KeyEventArgs e)
     {
         if (e.Key == Key.Enter)  (DataContext as dynamic)?.ConfirmCommand.Execute(null);
         if (e.Key == Key.Escape) (DataContext as dynamic)?.CancelCommand.Execute(null);
